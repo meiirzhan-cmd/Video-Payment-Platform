c34b6cb11f31a2ee65a7986d2e64dd0cf4923a07
Meiirzhan Baitangatov <rikimarubro@gmail.com>
2026-02-08 17:27:25 +0500

    Spring boot initializer

---
+++ StreamApplication/src/test/java/com/LearnStreamApplication/StreamApplication/StreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LearnStreamApplication.StreamApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
